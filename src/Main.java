569c641620a580f5c6bd19f8fdf45afd14ac1886
Sergey Sergeychik <sergey.sergeychik@gmail.com>
2020-09-09 12:26:30 +0300

    Описание

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         // TODO: подставлять номер карты нужно сюда между двойными кавычками, без пробелов
-        String number = "30573570609610";
+        String number = "36866684561220";
         System.out.println(String.format("Result is %s", isValidCardNumber(number) ? "OK" : "FAIL"));
     }
 
